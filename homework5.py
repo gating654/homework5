a0618f3f67a3ed333341773c279159fdefa4d310
gating654 <168896707+gating654@users.noreply.github.com>
2024-05-10 13:41:57 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ["Дыня", "Арбуз", "Виноград", "Груша", "Яблоко"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = "Киви"
+print(my_list)
+my_dict = {"Hello":"Привет", "Homework":"Домашняя работа", "Goodbye":"До свидания"}
+print(my_dict)
+print(my_dict["Hello"])
+my_dict["Hello"] = "Здравствуйте"
+my_dict["Locked"] = "Заперто"
+print(my_dict)
